2c49de649bf52b1ea45ccd9a7ff8716445f119d1
JackLiu16 <2584316041@qq.com>
2018-11-30 06:04:16 +0800

    fix code

--- config.py
+++ config.py
@@ -3,6 +3,6 @@
 
 from __future__ import absolute_import
 
-CELERY_RESULT_BACKEND = 'redis://127.0. .1:6379/5'
+CELERY_RESULT_BACKEND = 'redis://127.0.0.1:6379/5'
 BROKER_URL = 'redis://127.0.0.1:6379/6'
 #BROKER_URL = 'amqp://guest:guest@127.0.0.1:5672/'
